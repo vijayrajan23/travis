da2a22a652a98a7a74821ad07254b25f9d201aef
vijayrajan23 <vijayrajan23@gmail.com>
2020-02-15 21:26:17 +0530

    change app name

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-           vijayrajan
+        <p classsName="App-intro">
+           Nila Application
         </p>
         <a
           className="App-link"
